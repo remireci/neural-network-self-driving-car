cc89a61fb3734ebf8f29a57ae828f5be9ac8ad32
remireci <113353082+remireci@users.noreply.github.com>
2023-03-23 10:51:27 +0100

    initial project

---
+++ README.md
@@ -0,0 +1,12 @@
+# neural-network-self-driving-car
+
+The 'self-driving car' is a creation by Radu Mariescu Istodor.
+
+You can find a more sophisticated version on 
+        https://radufromfinland.com/
+
+More importantly, you can learn how to build it yourself in his great free course on 
+        https://www.freecodecamp.org/news/self-driving-car-javascript/ 
+In this course you get introduced in neural networks and genetic algorithms.
+
+To make the code work, I had to refactor sensor.js (added an if-statement) and road.js (changed a for-loop). Check the files.
---
+++ index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+const app = express();
+import bd from "body-parser";
+import ejs from 'ejs';
+
+const PORT = process.env.PORT || 8080;
+
+let numberOfCars;
+
+app.set("view engine", "ejs");
+app.use(bd.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+app.get("/", function (req, res) {
+    numberOfCars = 100;
+    res.render("index", {
+        number: numberOfCars,
+        numbertwo: true
+    });
+})
+
+app.get("/your_number_of_cars", function (req, res) {
+    res.render("index", {
+        number: numberOfCars,
+        numbertwo: false
+    });
+})
+
+app.post("/", function (req, res) {
+    numberOfCars = Number(req.body.number);
+    let check = req.body.check;
+    if (numberOfCars === 100 || isNaN(numberOfCars) === true || check === 'false') {
+        res.redirect("/");
+    } else {
+        res.redirect("/your_number_of_cars");
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}...`)
+  });
